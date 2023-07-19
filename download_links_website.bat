554b0f4635db8f80805333110927e03bed5e768a
Cesar A. Rivas <crivasg@gmail.com>
2023-07-18 18:24:25 -0600

    Update download_links_website.bat

--- download_links_website.bat
+++ download_links_website.bat
@@ -17,19 +17,27 @@ if not exist ".venv\Scripts\activate.bat" (
 	call %~dp0\init.bat 
 )
 
-call .venv\Scripts\activate.bat
+CALL .venv\Scripts\activate.bat
+IF -%1- == -- (
+	python -m download_links_website --help
+	CALL .venv\Scripts\deactivate.bat
+	EXIT /b 1
+)
+
 IF -%2- == -- (
 	python -m download_links_website --url "%1"
 ) ELSE (
 	python -m download_links_website --url "%1" --dest_folder "%2"
 )
+CALL .venv\Scripts\deactivate.bat
 
+ECHO.
 
-call .venv\Scripts\deactivate.bat
-
-echo.
-
-
+EXIT /b 0
 
 :: https://www.dynaexamples.com/implicit/yaris-static-suspension-system-loading/zip-for-download.zip
 :: https://www.steel.org/steel-markets/automotive/gdis/presentations/?_kw=&_author=&_submitter=&_category=&_title=&_year=2022&_track=&_session=&_organization=
+
+
+:: References:
+:: - https://stackoverflow.com/a/6135689
