24890a91a6d0def35d7d1823c336de9e0f8b6c85
Cesar A. Rivas <crivasg@gmail.com>
2023-07-18 18:43:05 -0600

    Update __main__.py

--- download_links_website/__main__.py
+++ download_links_website/__main__.py
@@ -2,18 +2,21 @@
 
 from download_links_website.cli import read_user_cli_args
 from download_links_website.web_requests import request_json_url, download_file, parse_url
-
+import os
 
 def main():
+
     user_args = read_user_cli_args()
     url = user_args.url
     dest_folder =  user_args.dest_folder
-    print(url)
-    print(dest_folder)
-    #parse_url(url=url)
+    
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder, exist_ok=True)
     
     link_urls = parse_url(url)
-    print(f'{link_urls=}')
+    for link_url in link_urls:
+        print(link_url)
+        download_file(link_url, dest_folder=dest_folder)
     
 
 if __name__ == '__main__':
