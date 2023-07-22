f2e09a4cfa8e9a85eca3f48a2eead02deb964c62
Cesar A. Rivas <crivasg@gmail.com>
2023-07-21 19:49:48 -0600

    Update download_links_website.bat

--- download_links_website.bat
+++ download_links_website.bat
@@ -25,9 +25,9 @@ IF -%1- == -- (
 )
 
 IF -%2- == -- (
-	python -m download_links_website --url "%1"
+	python -m download_links_website --input "%1"
 ) ELSE (
-	python -m download_links_website --url "%1" --dest_folder "%2"
+	python -m download_links_website --input "%1" --dest_folder "%2"
 )
 CALL .venv\Scripts\deactivate.bat
 
@@ -36,7 +36,9 @@ ECHO.
 EXIT /b 0
 
 :: https://www.dynaexamples.com/implicit/yaris-static-suspension-system-loading/zip-for-download.zip
-:: https://www.steel.org/steel-markets/automotive/gdis/presentations/?_kw=&_author=&_submitter=&_category=&_title=&_year=2022&_track=&_session=&_organization=
+:: https://www.dynaexamples.com/dynaexamples/thermal/welding-new/welding-solids/exp_welding_solids-tar.gz
+:: https://www.steel.org/steel-markets/automotive/gdis/presentations/?_kw=^&_author=^&_submitter=^&_category=^&_title=^&_year=2023^&_track=^&_session=^&_organization=
+:: https://www.beta-cae.com/resources.htm
 
 
 :: References:
