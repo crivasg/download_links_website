e49d03851ff5f5bc73545e3970b0c51c4bc98de8
Cesar A. Rivas <crivasg@gmail.com>
2023-07-29 09:48:56 -0600

    Add files via upload

---
+++ update_requirements.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal
+
+cd /d %~dp0
+
+echo #............................................................#
+echo #                                                            #
+echo #                                                            #
+echo #    Download_Links_from Website Upgrade                     # 
+echo #                                                            #
+echo #                                                            #
+echo #............................................................#
+echo.
+
+
+if not exist ".venv\Scripts\activate.bat" (
+	echo.
+	echo The virtal enviroment does not exist. 
+	echo Will try to make the enviroment.
+	echo.
+	
+	python -m venv .venv
+)
+
+call .venv\Scripts\activate.bat
+if exist requirements.txt (
+	python -m pip install --upgrade -r requirements.txt
+)
+call .venv\Scripts\deactivate.bat
+
+echo.
+echo Done .....
+echo.
